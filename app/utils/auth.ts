ab9d6c19b96ada5f0eb4c44642270dc278ed3e0f
devb0x <moniezgeoffrey59@gmail.com>
2023-12-29 10:37:24 +0100

    add next auth

---
+++ app/utils/auth.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "../api/mongodb"
+
+export const { handlers, auth } = NextAuth({
+	providers:
+		[GitHub({
+			allowDangerousEmailAccountLinking: true,
+		}), GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET,
+			allowDangerousEmailAccountLinking: true,
+		})],
+	pages: {
+		signIn: "/auth/signin"
+	},
+	adapter: MongoDBAdapter(clientPromise),
+	callbacks: {
+		async session({session, user}) {
+			session.user.id = user.id
+			return session
+		}
+	}
+})
