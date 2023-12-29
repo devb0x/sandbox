57d5b8b9ea22a827eac23ddf3eb1b1ad9b28cdd0
devb0x <moniezgeoffrey59@gmail.com>
2023-12-29 10:40:58 +0100

    update nextconfig for ignoring typescript error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	experimental: {
+		serverComponentsExternalPackages: ['mongoose']
+	},
+	typescript: {
+		ignoreBuildErrors: true
+	}
+}
 
 module.exports = nextConfig
